a0b6132c44017dbf2a5d4bf9e641a1e870e78ef2
Rob O'Gara <rgo594@gmail.com>
2019-03-18 15:04:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(a1, a2)
 end
 
 def using_insert(array, element)
-  #it uses the insert method to add the new element to the 4th index of the array
+  array[3].insert(element)
 end
 
 def using_uniq(array)
