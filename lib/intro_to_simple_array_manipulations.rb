af5b94a1f1308cb15bcb9e3c4492be3d1899526c
Rob O'Gara <rgo594@gmail.com>
2019-03-18 15:10:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,6 +39,7 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
+  array.delete(string)
   #uses thedelete method to remove any items from the array that are equal to that string
 end
 
