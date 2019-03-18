a396e93223ed389400e534c29c33e947cc17eda1
Rob O'Gara <rgo594@gmail.com>
2019-03-18 14:50:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,13 +3,13 @@ def using_push(array, string)
 end
 
 def using_unshift(array, string)
-  
+  #inc length of array
 end
 
 def using_pop(array)
-  dec array by 2
+  #dec array by 1
 end
 
 def pop_with_args(array)
-  
+  #uses the pop method with an argument of 2 to remove the last two array items and return them
 end
